0a2bb485af1ca8e6bca4ec359734561182d31872
tuannhm72 <113976381+tuannhm72@users.noreply.github.com>
2023-02-13 21:19:21 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+const nameGirl = 'Nhi';
+const giftUrl1 = 'https://www.messenger.com/t/100033050988778/';
+const eventName1 = 'Chúc Mừng Sinh Nhật';
+const titleCard1 = 'Happy Birthday';
+const contentCard1 = 'Happy Birthday Make A Wish. Dear brother, hope you receive everything you wish for this year. You deserve the best! Thanks for being such a great brother.💘';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+
+const giftImage = 'image/image3-1.jpg'; //image3
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
